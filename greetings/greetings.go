5a226687cb4bff2ad40e6e20fc489c98d3577fa5
Kartikey Srivastava <kartikey.srivastava1271@gmail.com>
2025-08-23 20:44:01 +0530

    error handling

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,15 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello the function name is in capital letter so it could be called from outside this package
-func Hello(name string) string {
-	// return a greeting that embeds name in a message
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
